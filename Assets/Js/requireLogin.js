6b2bdee21c89a2f5c2402e1557f29ce64cc5579e
isiahmacias25 <151044963+isiahmacias25@users.noreply.github.com>
2025-11-01 18:40:24 -0400

    Create requireLogin.js

---
+++ Assets/Js/requireLogin.js
@@ -0,0 +1,22 @@
+// Firebase Initialization
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
+import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCTIK0ulR0uSHoiPBoK4Fymutw81sM7H_8",
+  authDomain: "behindthebar.firebaseapp.com",
+  projectId: "behindthebar",
+  storageBucket: "behindthebar.firebasestorage.app",
+  messagingSenderId: "134727677191",
+  appId: "1:134727677191:web:6c15d2e83196abd36b81a2"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+// Require Login Check
+onAuthStateChanged(auth, (user) => {
+  if (!user) {
+    window.location.href = "index.html";
+  }
+});
